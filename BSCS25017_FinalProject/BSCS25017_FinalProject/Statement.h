05431ddeb1245cfe044eb429c455306fd0855d62
m-abdullah-2007 <1005hafizabdullah@gmail.com>
2026-06-14 15:21:53 +0500

    Added Statement Heirarchy for different Operations

---
+++ BSCS25017_FinalProject/BSCS25017_FinalProject/Statement.h
@@ -0,0 +1,111 @@
+#pragma once
+#include "expression.h"
+using namespace std;
+class Environment; 
+
+
+class Statement {
+    Statement* next;
+public:
+    Statement() : next(nullptr) {}
+    virtual ~Statement() {}
+    virtual void execute(Environment& env) = 0;
+};
+
+
+
+class PrintStmt : public Statement {
+    Expression* expr;
+public:
+    PrintStmt(Expression* e) : expr(e) {}
+    void execute(Environment& env) override {
+        Value* result = expr->evaluate(env);
+        cout << result->to_string() << std::endl;
+        delete result;
+    }
+};
+
+
+class LetStmt : public Statement {
+public:
+    MyString name;
+    Expression* init;
+    LetStmt(MyString n, Expression* i) : name(n), init(i) {}
+    void execute(Environment& env) override {
+        Value* value = init->evaluate(env);
+        env.define(name, value);
+    }
+};
+
+class AssignStmt : public Statement {
+public:
+    MyString name;
+    Expression* value;
+    int line;
+
+    AssignStmt(MyString n, Expression* v, int l): name(n), value(v), line(l) {}
+    void execute(Environment& env) override {
+        Value* new_value = value->evaluate(env);
+        env.assign(name, new_value);
+    }
+};
+
+
+class IfStmt : public Statement {
+public:
+    Expression* condition;
+    Statement* then_branch;
+    Statement* else_branch;
+
+    IfStmt(Expression* c, Statement* t, Statement* e = nullptr): condition(c), then_branch(t), else_branch(e) {}
+    void execute(Environment& env) override {
+        Value* result = condition->evaluate(env);
+        if (result->is_truthy()) {
+            then_branch->execute(env);
+        }
+        else if (else_branch != nullptr) {
+            else_branch->execute(env);
+        }
+        delete result;
+    }
+};
+
+
+
+class WhileStmt : public Statement {
+public:
+    Expression* condition;
+    Statement* body;
+
+    WhileStmt(Expression* c, Statement* b) : condition(c), body(b) {}
+    void execute(Environment& env) override {
+        while (true) {
+            Value* result = condition->evaluate(env);
+            bool cont = result->is_truthy();
+            delete result;
+
+            if (!cont) {
+                break;
+            }
+
+            body->execute(env);
+        }
+    }
+};
+
+
+class BlockStmt : public Statement {
+public:
+    Statement* first;
+
+    BlockStmt(Statement* f) : first(f) {}
+    void execute(Environment& env) override {
+        Environment child_env = env.create_child();
+
+        Statement* current = first;
+        while (current != nullptr) {
+            current->execute(child_env);
+            current = current->next;
+        }
+    }
+};
--- BSCS25017_FinalProject/BSCS25017_FinalProject/Value.h
+++ BSCS25017_FinalProject/BSCS25017_FinalProject/Value.h
@@ -1,5 +1,6 @@
 #pragma once
 #include"MyString.h"
+#include<iostream>
 
 class Value {
 	
